d7d4490752f2208b6764ecf0e1198eb5fec48cec
Pavel Anni <pavel.anni@gmail.com>
2020-12-25 20:16:33 -0500

    Add go.mod to ch2

---
+++ ch2/go.mod
@@ -0,0 +1,5 @@
+module github.com/pavelanni/gopl.io/ch2
+
+go 1.15
+
+require github.com/pavelanni/gopl.io/ch2/tempconv v0.0.0-20201226004317-5e892b85894b
--- ch2/tempconv/conv.go
+++ ch2/tempconv/conv.go
@@ -10,4 +10,4 @@ func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
 func CToK(c Celsius) Kelvin { return Kelvin(c - AbsoluteZeroC) }
 
 // KToC converts Kelvin to Celsius
-func KToC(k Kelvin) Celsius { return Celsius(k + AbsoluteZeroC) }
+func KToC(k Kelvin) Celsius { return Celsius(k + Kelvin(AbsoluteZeroC)) }
